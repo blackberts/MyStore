4f14a5530823c84303b9ee22585cf03de04c74ac
blackberts13@gmail.com <blackberts13@gmail.com>
2022-06-25 19:22:31 +0300

    Сегодня попробовал поработать с созданием бд непосредственно в самой Visual Studio, прикольный опыт, учитывая тот факт, как надо добавлять данные и что для этого надо писать. В общем пока что добавил базовое меню с клиентами и их данными, написал логику на создание нового клиента и добавление его в бд.

--- MyStore/Pages/Clients/Index.cshtml.cs
+++ MyStore/Pages/Clients/Index.cshtml.cs
@@ -1,12 +1,60 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
 
 namespace MyStore.Pages.Clients
 {
     public class IndexModel : PageModel
     {
+        public List<ClientInfo> listClients = new List<ClientInfo>();
         public void OnGet()
         {
+            try
+            {
+                // connection to db
+                string connectionString = "Data Source=DESKTOP-GUM1BKT;Initial Catalog=mystore;Integrated Security=True";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    // open table with syntaxys SELECT * FROM clients
+                    connection.Open();
+                    string sql = "SELECT * FROM clients";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using(SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                ClientInfo clientInfo = new ClientInfo();
+                                clientInfo.id = "" + reader.GetInt32(0);
+                                clientInfo.name = reader.GetString(1);
+                                clientInfo.email = reader.GetString(2);
+                                clientInfo.phone = reader.GetString(3);
+                                clientInfo.address = reader.GetString(4);
+                                clientInfo.created_at = reader.GetDateTime(5).ToString();
+
+                                listClients.Add(clientInfo);
+                            }
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
+    }
+
+    // this is a class which saving clients info
+    public class ClientInfo
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+        public string created_at { get; set; }
     }
 }
